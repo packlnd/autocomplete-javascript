d230a1b7c2578e10b61ac156dbd09249c91b2d15
Patrik Ackland <patrikackland@gmail.com>
2016-02-10 21:53:59 -0800

    Very simple way to extract (first) <code> tag.

--- prog.py
+++ prog.py
@@ -1,10 +1,13 @@
 from parse_xml import get_data
+from BeautifulSoup import BeautifulSoup as BSHTML
 
 """ Code is between <code> tags. """
 def get_code(desc):
-    raise NotImplementedError
+    bs = BSHTML(desc)
+    return bs.code
 
 data = get_data('PostsMin.xml')
 post = data[0]
 desc = post.attributes['Body'].value
-get_code(desc)
+code = get_code(desc)
+print code
