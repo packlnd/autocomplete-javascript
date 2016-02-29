d9ebbcc5cab3ee82f48bb2970c2fc066d494030d
Jennifer Niederlaender <jennifer.mail1@gmail.com>
2016-02-29 11:46:33 -0800

    plugin with feedback and better showing of the results

--- effTemp/effTemp.py
+++ effTemp/effTemp.py
@@ -6,6 +6,11 @@ import functools
 import urllib2
 import json
 
+try:
+    from functools import reduce
+except:
+    pass
+
 class SnippetCommand(sublime_plugin.TextCommand):
 
 
@@ -18,6 +23,7 @@ class SnippetCommand(sublime_plugin.TextCommand):
         def on_done(text):
             # call the method that returns the code snippets
             text = text.replace(' ', '%20')
+            query = text
             text = 'http://recycle.cs.washington.edu:5000/search?query=' + text + '&token=cse504'
             f = urllib2.urlopen(text)
             choices = []
@@ -29,15 +35,11 @@ class SnippetCommand(sublime_plugin.TextCommand):
                 code = ' '.join(code)
                 # code = code.replace('\n', ' ')
                 choices.append(code)
-
-
-                
-
-
             
             self.view.run_command('snippet_selection_helper', {
                 'args': {
-                    'choices': choices
+                    'choices': choices,
+                    'query' : query
                 }
             })
 
@@ -50,23 +52,33 @@ class SnippetSelectionHelperCommand(sublime_plugin.TextCommand):
         """Called when the command is run."""
         self.choices = args['choices']
         self.edit = edit
+        self.query = args['query']
+
+        self.selectify(self.choices)
 
         sublime.active_window().show_quick_panel(
-            self.choices, self.on_done_call_func(self.choices))
+            self.choices, self.on_done_call_func(args['choices']))
+
+    def selectify(self, choices):
+        tuples = [(idc, choice.split('\n')) for idc,choice in enumerate(choices)]
+        self.mapping = reduce(lambda a,b:a+[None]+b, [[idc]*len(lines) for (idc,lines) in tuples])
+        self.choices = reduce(lambda a,b:a+['--------------------------------']+b, [lines for (idc,lines) in tuples])
 
     def on_done_call_func(self, choices):
         """Return a function which is used with sublime list picking."""
         def on_done(index):
-            if index >= 0:
-                return self.insert(choices[index])
+            if index >= 0 and self.mapping[index] is not None:
+                return self.insert(choices[self.mapping[index]],self.mapping[index])
 
         return on_done
 
-    def insert(self, choice):
+    def insert(self, choice, index):
         self.view.run_command('snippet_insert_helper', {
             'args': {
             #TODO: return choice to the learning algorithm
-                'choice': choice
+                'choice': choice,
+                'index': index,
+                'query': self.query
             }
         })
 
@@ -77,3 +89,5 @@ class SnippetInsertHelperCommand(sublime_plugin.TextCommand):
         content = choice 
         position = self.view.sel()[0].begin()
         self.view.insert(edit, position, content)
+
+        urllib2.urlopen('http://recycle.cs.washington.edu:5000/feedback?query='+args['query']+'&token=cse504&docid='+str(args['index']))
