53a68ea7850e801740549e91c9e35955b06ee840
Nicholas Reiter <nreiter@recycle.cs.washington.edu>
2016-03-06 13:56:08 -0800

    Adding es part, updating so api

---
+++ feedback.py
@@ -0,0 +1,98 @@
+from elasticsearch import Elasticsearch
+from flask import Flask
+from flask import request
+import json
+import so_api
+
+app = Flask(__name__)
+
+@app.route('/feedback')
+def record_feedback():
+    query = request.args.get('query', None)
+    docid = request.args.get('docid', -1)
+    token = request.args.get('token', None)
+    if query is None or docid < 0 or token != 'cse504':
+        return 'Query and docid must be set.'
+
+    es = get_es()
+
+    # Need to get the document if it exists, and update
+    # the keywords field
+    curr_feedback = es.get(index='feedback', doc_type='event', id=docid, ignore=[404])
+
+    body = [query]
+    if curr_feedback['found']:
+        body.append(curr_feedback['_source']['keywords'])
+
+    event = {
+        'keywords': ' '.join(body)
+    }
+
+    es.index(
+        index='feedback',
+        doc_type='event',
+        id=docid,
+        body=event
+    )
+
+    return 'OK'
+
+@app.route('/inspect')
+def inspect_document():
+    docid = request.args.get('docid', -1)
+    token = request.args.get('token', None)
+    if docid < 0 or token != 'cse504':
+        return 'Docid must be set.'
+
+    es = get_es()
+
+    # Need to get the document if it exists, and update
+    # the keywords field
+    curr_feedback = es.get(index='feedback', doc_type='event', id=docid, ignore=[404])
+
+    if curr_feedback['found']:
+        return str(curr_feedback['_source']['keywords'])
+    else:
+        return 'Not found'
+
+def get_es():
+    return Elasticsearch(
+        [
+            {
+                'host': 'localhost',
+                'port': 9292
+            }
+        ],
+        http_auth=('cse504', 'JNPP@ss1')
+    )
+
+@app.route('/test')
+def test():
+    return 'Hello world!'
+
+@app.route('/search')
+def search_feedback():
+    query = request.args.get('query', None)
+    token = request.args.get('token', None)
+    if query is None or token != 'cse504':
+        return 'Query cannot be empty.'
+
+    es = get_es()
+
+    search = {
+        'query': {
+            'match': {
+                'keywords': query
+            }
+        }
+    }
+
+    results = es.search(index='feedback', doc_type='event', body=search, size=5, _source=False)
+
+    ids = [int(res['_id']) for res in results['hits']['hits']]
+
+    # Now query SO for more results
+    return json.dumps(so_api.query(query, ids))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- so_api.py
+++ so_api.py
@@ -10,14 +10,10 @@ def so_request(ep, params={}):
         url += '?'
         url += '&'.join(['%s=%s' % (k,v)
             for k,v in params.iteritems()])
-    print url
     return requests.get(url).json()
 
 def collect_ids(json_data):
-    ids = []
-    for j in json_data:
-        ids.append(j['question_id'])
-    return ids
+    return [j['accepted_answer_id'] for j in json_data]
 
 def filter_data(data):
     #TODO: Currently no filter
@@ -27,40 +23,54 @@ def get_questions(q):
     params = {
         'q':q,
         'views':100,
-        'accepted':'true',
+        'accepted':True,
         'site':'stackoverflow',
         'filter':'withbody',
         'sort':'relevance'
     }
     req = so_request("search/advanced", params)
-    return req['items']
+    return req['items']#  Could limit to 10 here, but empty answers mess with that: [:10]
 
 def get_answers(ids):
     format_ids = ';'.join([str(i) for i in ids])
     params = {
         'site':'stackoverflow',
-        'filter':'withbody'
+        'filter':'withbody',
+        'pagesize': 50
     }
-    req = so_request('questions/' + format_ids + '/answers', params)
-    return req['items']
+    req = so_request('answers/' + format_ids, params)
+    id_mapping = {item['answer_id']: item for item in req['items']}
+    return [id_mapping[a_id] for a_id in ids if a_id in id_mapping]
 
 def get_code(body):
     soup = BeautifulSoup(body)
-    return soup.findAll('code')
+    return [code.getText() for code in soup.findAll('code')]
 
 def get_list_of_code(answers):
-    return [get_code(a['body']) for a in answers]
+    code = [[a['answer_id'], get_code(a['body'])] for a in answers]
+    return code
 
 def prepare_query(q):
-    return q.replace(' ', '+')
+    return q.replace(' ', '%20')
+
+def remove_empty(code):
+    return [c for c in code if len(c[1]) > 0][:10]
 
 def query_so(q):
     # Current approach gives answers to all SO questions matching search query
     q = prepare_query(q)
     json_data = get_questions(q)
     filtered = filter_data(json_data)
-    ids = collect_ids(filtered)
-    answers = get_answers(ids)
-    return get_list_of_code(answers)
+    # Return the ids of the accepted answers
+    return collect_ids(filtered)
 
-query_so("get query parameters")
+# Remove any ids from the SO results that we have from feedback
+def reconcile_ids(f_ids, s_ids):
+  f_id_set = set(f_ids)
+  return f_ids + [s_id for s_id in s_ids if s_id not in f_id_set]
+
+def query(q, feedback_ids):
+    so_ids = query_so(q)
+    ids = reconcile_ids(feedback_ids, so_ids)
+    answers = get_answers(ids)
+    return remove_empty(get_list_of_code(answers))
